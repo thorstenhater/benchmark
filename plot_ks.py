949f3ea9551b6446072e44428b87c6c523227a90
abiakarn <nora.abiakar@gmail.com>
2020-10-14 14:28:56 +0200

    add new config

--- plot_ks.py
+++ plot_ks.py
@@ -5,21 +5,21 @@ import pandas as pd
 max_y = []
 out = []
 
-streams              = [1, 2, 4, 8, 16, 32]
-kernels_per_stream   = [1, 2, 4, 8, 16, 32]
+kernels   = [1, 2, 4, 8]
+streams   = [8, 16, 32, 64]
 arr_sizes = [10, 15, 20]
 
 for a in arr_sizes: 
   out.append([])
   max_y.append([])
-  for k in kernels_per_stream:
-    filename = 'results_s/out_s_'+str(a)+'_'+str(k)+'.csv'
+  for k in kernels:
+    filename = 'results_ks/out_ks_'+str(a)+'_'+str(k)+'.csv'
     csv = pd.read_csv(filename) 
     out[-1].append(csv)
     max_y[-1].append(max(csv["throughput(MB/s)"]))
 
 
-fig, ax = plt.subplots(nrows=len(arr_sizes), ncols=len(kernels_per_stream))
+fig, ax = plt.subplots(nrows=len(arr_sizes), ncols=len(kernels))
 
 for r in range(len(ax)):
   for c in range(len(ax[r])):
@@ -32,6 +32,6 @@ for r in range(len(ax)):
     p.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
     p.set_ylabel('Throughput MB/s', fontsize='small')
     p.set_xlabel('streams', fontsize='small')
-    p.set_title('size = ' + str(2<<arr_sizes[r]) + ' bytes; kernels_per_stream= '+ str(kernels_per_stream[c]), fontsize='small')
+    p.set_title('size = ' + str(2<<arr_sizes[r]) + ' bytes; total kernels= '+ str(kernels[c]), fontsize='small')
 
 plt.show()
